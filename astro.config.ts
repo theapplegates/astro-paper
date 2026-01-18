3bfacca67644865250bab377c863913d7e06de78
Paul Applegate <paul@theapplegates.com>
2026-01-18 16:48:44 -0500

    sss

--- astro.config.ts
+++ astro.config.ts
@@ -13,7 +13,7 @@ import { SITE } from "./src/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: https://paper.paulapplegate.com,
+  site: paper.paulapplegate.com,
   integrations: [
     sitemap({
       filter: page => SITE.showArchives || !page.endsWith("/archives"),
