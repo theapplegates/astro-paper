fd229f96f98ab8e042c5ce29eada3f14c9c4a8b6
Paul <paul@theapplegates.com>
2026-09-08 11:19:37 -0400

    Add Wotfard local font family

--- src/pages/posts/[...slug]/index.png.tsx
+++ src/pages/posts/[...slug]/index.png.tsx
@@ -27,7 +27,7 @@ export const GET: APIRoute = async ({ props, url }) => {
     return new Response(null, { status: 404, statusText: "Not found" });
   }
 
-  const fonts = fontData["--font-google-sans-code"];
+  const fonts = fontData["--font-wotfard"];
   const regularFontPath = getFontPathByWeight(fonts, 400);
   const boldFontPath = getFontPathByWeight(fonts, 700);
 
@@ -132,13 +132,13 @@ const svg = await satori(
       embedFont: true,
       fonts: [
         {
-          name: "Google Sans Code",
+          name: "Wotfard",
           data: regularData,
           weight: 400,
           style: "normal",
         },
         {
-          name: "Google Sans Code",
+          name: "Wotfard",
           data: boldData,
           weight: 700,
           style: "normal",
